05bb70e44bb4321df7d7e10359fd1a2059014079
zhangdingqu <zhangdingqu6@gmail.com>
2018-09-26 00:18:12 +0800

    增加了selenium笔记-增加了一点笔记.py

--- 爬虫工具以及笔记/selenium笔记.py
+++ 爬虫工具以及笔记/selenium笔记.py
@@ -15,12 +15,30 @@ driver.find_element_by_tag_name("input")
 driver.find_element_by_link_text("新闻很长的文本很长的文本")#获取新闻的链接[完全匹配]
 driver.find_element_by_partial_link_text("新闻很长的")#部分文字匹配到获取到链接[部分匹配]
 
+重点//难点
+1.普通定位
+
+<input id="kw" class="s_ipt" type="text" autocomplete="off" maxlength="100" name="wd">
 
 driver.find_element_by_xpath('//*[@id="kw"]')
 driver.find_element_by_xpath('//*[@name="wd"]')
 driver.find_element_by_xpath('//*[@class="s_ipt"]')
 driver.find_element_by_xpath('//*[@class="s_ipt"]')
+driver.find_element_by_xpath('//input[@maxlength="100"]') # 自定义里面任何属性都可以用，但是要是唯一的！
+driver.find_element_by_xpath('//input') # 只要有就找到
 
+2.示例html层级 [xpath方式]
+
+<div class="s_form_wrapper">
+    <form id="form" class="fm" action="/s" name="f">
+        <span class="bg">
+            <input class="s_ipt" autocomplete="off" maxlength="255" abc="123"
+                value="" name="wd">
+                
+driver.find_element_by_xpath('//span[@class="bg"]/input') # 定位到【class=bg】的元素里面的【input】标签
+driver.find_element_by_xpath('//form[@id="form"]/span/input') # 定位到【id="form"】的元素里面的【span/input】标签
+
+       
 driver.find_element_by_css_selector()
 
 '''
